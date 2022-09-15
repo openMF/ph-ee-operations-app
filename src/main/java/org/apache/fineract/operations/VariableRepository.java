d912fc0f6b5084eb01ad5abc747e99fdf6196ad6
DanishJamal <danishjamal.104@gmail.com>
2022-09-15 18:06:17 +0530

    2x data in report generation logic fixed

--- src/main/java/org/apache/fineract/operations/VariableRepository.java
+++ src/main/java/org/apache/fineract/operations/VariableRepository.java
@@ -1,11 +1,17 @@
 package org.apache.fineract.operations;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface VariableRepository extends CrudRepository<Variable, Long> {
 
     List<Variable> findByWorkflowInstanceKeyOrderByTimestamp(Long workflowInstanceKey);
+
+    @Query("SELECT v from Variable v WHERE v.workflowInstanceKey=:workflowInstanceKey and v.name=:name")
+    Variable findByWorkflowInstanceKeyAndVariableName(@Param("name") String name,
+                                                      @Param("workflowInstanceKey")  Long workflowInstanceKey);
 
 }
