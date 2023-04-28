0e6ddd88d39ec43f003196fa32b75785638aa795
truthfool <ishanranasingh@gmail.com>
2023-04-28 15:14:04 +0530

    logging date and time

--- src/main/java/org/apache/fineract/utils/DateUtil.java
+++ src/main/java/org/apache/fineract/utils/DateUtil.java
@@ -15,13 +15,21 @@ public class DateUtil {
     public String interfaceTimezone;
 
     public String getUTCFormat(String dateTime) {
+        logger.info("Inside GET UTC Method 1");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        logger.info("Inside GET UTC Method 2");
         LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+        logger.info("Inside GET UTC Method 3");
         ZoneId interfaceZone = ZoneId.of(interfaceTimezone);
+        logger.info("Inside GET UTC Method 4");
         ZonedDateTime interfaceDateTime = ZonedDateTime.of(localDateTime, interfaceZone);
+        logger.info("Inside GET UTC Method 5");
         ZonedDateTime gmtDateTime = interfaceDateTime.withZoneSameInstant(ZoneId.of("GMT"));
+        logger.info("Inside GET UTC Method 6");
         DateTimeFormatter gmtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        logger.info("Inside GET UTC Method 7");
         String gmtDateTimeString = gmtDateTime.format(gmtFormatter);
+        logger.info("Inside GET UTC Method 8");
         logger.info("New Date time : {}",gmtDateTimeString);
         return gmtDateTimeString;
     }
