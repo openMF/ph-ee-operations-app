ee53086c121c403cbf1d5bee6c79e89edbadb1df
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-07-03 12:28:37 +0200

    implement search by internalAccountId

--- src/main/java/org/apache/fineract/operations/TransferRepository.java
+++ src/main/java/org/apache/fineract/operations/TransferRepository.java
@@ -1,22 +1,12 @@
 package org.apache.fineract.operations;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface TransferRepository extends JpaRepository<Transfer, Long>, JpaSpecificationExecutor {
 
     Transfer findFirstByWorkflowInstanceKey(Long workflowInstanceKey);
 
     Transfer findFirstByTransactionIdAndDirection(String transactionId, String direction);
-
-    List<Transfer> findAllByBatchId(String batchId);
-
-    Page<Transfer> findAllByBatchIdAndStatus(String batchId, String status, Pageable pageable);
-
-    Page<Transfer> findAllByBatchId(String batchId, Pageable pageable);
 
 }
