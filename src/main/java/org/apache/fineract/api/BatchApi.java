59b82d96de312bc612de7aaca7bc2786bc7aaf37
DanishJamal <danishjamal.104@gmail.com>
2023-08-04 16:34:44 +0530

    Added regsiteringInstitutionId and payerFso request para support

--- src/main/java/org/apache/fineract/api/BatchApi.java
+++ src/main/java/org/apache/fineract/api/BatchApi.java
@@ -100,7 +100,12 @@ public class BatchApi {
                                    @RequestParam(value = "sort", required = false, defaultValue = "+completedAt")
                                        String sort,
                                 @RequestParam(value = "dateFrom", required = false) String startFrom,
-                                @RequestParam(value = "dateTo", required = false) String startTo) {
+                                @RequestParam(value = "dateTo", required = false) String startTo,
+                                @RequestParam(value = "registeringInstitutionId", required = false, defaultValue = "") String registeringInstituteId,
+                                @RequestParam(value = "payerFsp", required = false, defaultValue = "") String payerFsp) {
+        registeringInstituteId = registeringInstituteId + "%";
+        payerFsp = payerFsp + "%";
+        log.info("Registering Id: {}, PyaerFsp: {}", registeringInstituteId, payerFsp);
         Sort sortObject = getSortObject(sort);
         int page = Math.floorDiv(offset, limit);
         PageRequest pager = PageRequest.of(page, limit, sortObject);
@@ -122,69 +127,69 @@ public class BatchApi {
             if (startFrom != null && startTo != null) {
                 totalTransactions = batchRepository.getTotalTransactionsDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalAmount = batchRepository.getTotalAmountDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalBatches = batchRepository.getTotalBatchesDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedCount = batchRepository.getTotalApprovedCountDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedAmount = batchRepository.getTotalApprovedAmountDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                batches = batchRepository.findAllFilterDateBetween(
                         dateFormat().parse(startFrom), dateFormat().parse(startTo),
-                        "%", "%",
+                       registeringInstituteId, payerFsp,
                         pager);
             } else if (startFrom != null) {
                 log.info("Date: {}", startFrom);
                 totalTransactions = batchRepository.getTotalTransactionsDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalAmount = batchRepository.getTotalAmountDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalBatches = batchRepository.getTotalBatchesDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedCount = batchRepository.getTotalApprovedCountDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedAmount = batchRepository.getTotalApprovedAmountDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 batches = batchRepository.findAllFilterDateFrom(
                         dateFormat().parse(startFrom),
-                        "%", "%", pager);
+                        registeringInstituteId, payerFsp, pager);
             } else if (startTo != null) {
                 totalTransactions = batchRepository.getTotalTransactionsDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalAmount = batchRepository.getTotalAmountDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalBatches = batchRepository.getTotalBatchesDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedCount = batchRepository.getTotalApprovedCountDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 totalApprovedAmount= batchRepository.getTotalApprovedAmountDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%");
+                        registeringInstituteId, payerFsp);
                 batches = batchRepository.findAllFilterDateTo(
                         dateFormat().parse(startTo),
-                        "%", "%", pager);
+                        registeringInstituteId, payerFsp, pager);
             } else {
-                totalTransactions = batchRepository.getTotalTransactions("%", "%");
-                totalAmount = batchRepository.getTotalAmount("%", "%");
-                totalBatches = batchRepository.getTotalBatches("%", "%");
-                totalApprovedCount = batchRepository.getTotalApprovedCount("%", "%");
-                totalApprovedAmount = batchRepository.getTotalApprovedAmount("%", "%");
-                batches = batchRepository.findAllPaged("%", "%", pager);
+                totalTransactions = batchRepository.getTotalTransactions(registeringInstituteId, payerFsp);
+                totalAmount = batchRepository.getTotalAmount(registeringInstituteId, payerFsp);
+                totalBatches = batchRepository.getTotalBatches(registeringInstituteId, payerFsp);
+                totalApprovedCount = batchRepository.getTotalApprovedCount(registeringInstituteId, payerFsp);
+                totalApprovedAmount = batchRepository.getTotalApprovedAmount(registeringInstituteId, payerFsp);
+                batches = batchRepository.findAllPaged(registeringInstituteId, payerFsp, pager);
             }
         } catch (Exception e) {
             log.warn("failed to parse dates {} / {}", startFrom, startTo);
