244f8824c3cf8c183a44698251b15f5ed4a2d0e8
DanishJamal <danishjamal.104@gmail.com>
2023-08-04 15:52:26 +0530

    Bug fixed for NULL sum case

--- src/main/java/org/apache/fineract/operations/BatchPaginatedResponse.java
+++ src/main/java/org/apache/fineract/operations/BatchPaginatedResponse.java
@@ -11,4 +11,27 @@ public class BatchPaginatedResponse {
     long totalBatches, totalTransactions, totalAmount;
     List<Batch> data;
 
+    public void setTotalBatches(Long totalBatches) {
+        if (totalBatches != null) {
+            this.totalBatches = totalBatches;
+        } else {
+            this.totalBatches = 0;
+        }
+    }
+
+    public void setTotalTransactions(Long totalTransactions) {
+        if (totalTransactions != null) {
+            this.totalTransactions = totalTransactions;
+        } else {
+            this.totalTransactions = 0;
+        }
+    }
+
+    public void setTotalAmount(Long totalAmount) {
+        if (totalAmount != null) {
+            this.totalAmount = totalAmount;
+        } else {
+            this.totalAmount = 0;
+        }
+    }
 }
