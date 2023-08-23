06fb21951691dbc6b614a42b45112991c874d737
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-08-23 10:51:19 +0200

    add new recall fields to transfer table

--- src/main/java/org/apache/fineract/operations/Transfer.java
+++ src/main/java/org/apache/fineract/operations/Transfer.java
@@ -9,11 +9,14 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
 @Table(name = "transfers")
+@Data
 public class Transfer {
 
     @Id
@@ -80,6 +83,12 @@ public class Transfer {
     @Column(name = "ENDTOENDIDENTIFICATION")
     private String endToEndIdentification;
 
+    @Column(name = "RECALL_STATUS")
+    private String recallStatus;
+
+    @Column(name = "RECALL_DIRECTION")
+    private String recallDirection;
+
 
     public Transfer() {
     }
@@ -89,195 +98,4 @@ public class Transfer {
         this.status = TransferStatus.IN_PROGRESS;
     }
 
-    public Date getCompletedAt() {
-        return completedAt;
-    }
-
-    public void setCompletedAt(Date completedAt) {
-        this.completedAt = completedAt;
-    }
-
-    public Date getStartedAt() {
-        return startedAt;
-    }
-
-    public void setStartedAt(Date startedAt) {
-        this.startedAt = startedAt;
-    }
-
-    public BigDecimal getPayeeFee() {
-        return payeeFee;
-    }
-
-    public void setPayeeFee(BigDecimal payeeFee) {
-        this.payeeFee = payeeFee;
-    }
-
-    public String getPayeeQuoteCode() {
-        return payeeQuoteCode;
-    }
-
-    public void setPayeeQuoteCode(String payeeQuoteCode) {
-        this.payeeQuoteCode = payeeQuoteCode;
-    }
-
-    public BigDecimal getPayerFee() {
-        return payerFee;
-    }
-
-    public void setPayerFee(BigDecimal payerFee) {
-        this.payerFee = payerFee;
-    }
-
-    public String getPayerQuoteCode() {
-        return payerQuoteCode;
-    }
-
-    public void setPayerQuoteCode(String payerQuoteCode) {
-        this.payerQuoteCode = payerQuoteCode;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public Long getWorkflowInstanceKey() {
-        return workflowInstanceKey;
-    }
-
-    public void setWorkflowInstanceKey(Long paymentProcessId) {
-        this.workflowInstanceKey = paymentProcessId;
-    }
-
-    public TransferStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TransferStatus status) {
-        this.status = status;
-    }
-
-    public String getPayeePartyId() {
-        return payeePartyId;
-    }
-
-    public void setPayeePartyId(String payeePartyId) {
-        this.payeePartyId = payeePartyId;
-    }
-
-    public String getPayeePartyIdType() {
-        return payeePartyIdType;
-    }
-
-    public void setPayeePartyIdType(String payeePartyType) {
-        this.payeePartyIdType = payeePartyType;
-    }
-
-    public String getPayerPartyId() {
-        return payerPartyId;
-    }
-
-    public void setPayerPartyId(String payerPartyId) {
-        this.payerPartyId = payerPartyId;
-    }
-
-    public String getPayerPartyIdType() {
-        return payerPartyIdType;
-    }
-
-    public void setPayerPartyIdType(String payerPartyType) {
-        this.payerPartyIdType = payerPartyType;
-    }
-
-    public String getPayeeFeeCurrency() {
-        return payeeFeeCurrency;
-    }
-
-    public void setPayeeFeeCurrency(String payeeFeeCurrency) {
-        this.payeeFeeCurrency = payeeFeeCurrency;
-    }
-
-    public String getPayerFeeCurrency() {
-        return payerFeeCurrency;
-    }
-
-    public void setPayerFeeCurrency(String payerFeeCurrency) {
-        this.payerFeeCurrency = payerFeeCurrency;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getPayeeDfspId() {
-        return payeeDfspId;
-    }
-
-    public void setPayeeDfspId(String payeeDfspId) {
-        this.payeeDfspId = payeeDfspId;
-    }
-
-    public String getPayerDfspId() {
-        return payerDfspId;
-    }
-
-    public void setPayerDfspId(String payerDfspId) {
-        this.payerDfspId = payerDfspId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getStatusDetail() {
-        return statusDetail;
-    }
-
-    public void setStatusDetail(String statusDetail) {
-        this.statusDetail = statusDetail;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-
-    public String getErrorInformation() {
-        return errorInformation;
-    }
-
-    public void setErrorInformation(String errorInformation) {
-        this.errorInformation = errorInformation;
-    }
-
-    public String getBatchId() {
-        return batchId;
-    }
-
-    public void setBatchId(String batchId) {
-        this.batchId = batchId;
-    }
-
-    public String getEndToEndIdentification() {
-        return endToEndIdentification;
-    }
-
-    public void setEndToEndIdentification(String endToEndIdentification) {
-        this.endToEndIdentification = endToEndIdentification;
-    }
 }
