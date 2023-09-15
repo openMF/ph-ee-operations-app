739fc5d03fa9bb86124791d5f67a09ccdf2f014f
Dániel Gyenizse <daniel.gyenizse@binx.hu>
2023-09-15 15:29:24 +0200

    using comment as recallReason sent by admin ui

--- src/main/java/org/apache/fineract/core/service/CamundaService.java
+++ src/main/java/org/apache/fineract/core/service/CamundaService.java
@@ -4,6 +4,8 @@ import io.camunda.zeebe.client.ZeebeClient;
 import org.apache.fineract.operations.Transfer;
 import org.apache.fineract.operations.Variable;
 import org.apache.fineract.operations.VariableRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +34,7 @@ public class CamundaService {
     private VariableRepository variableRepository;
 
 
-    public void startRecallFlow(String paymentScheme, Transfer transfer) {
+    public void startRecallFlow(String requestBody, String paymentScheme, Transfer transfer) {
         String iban = getTransferVariable(transfer, "iban");
         String tenantIdentifier = getTransferVariable(transfer, "tenantIdentifier");
         String transactionGroupId = getTransferVariable(transfer, "transactionGroupId");
@@ -40,11 +42,18 @@ public class CamundaService {
         String transactionId = getTransferVariable(transfer, "transactionId");
         Map<String, String> variables = new HashMap<>();
         String bpmn;
+        String comment = null;
+        try {
+            JSONObject body = new JSONObject(requestBody);
+            comment = body.optString("comment", null);
+        } catch (Exception e) {
+            logger.error("Could not parse recall request body {}, can not set comment on recall!", requestBody);
+        }
         if ("HCT_INST".equalsIgnoreCase(paymentScheme)) {
             bpmn = recallBpmnInstant;
             variables.put("originalPacs008", getTransferVariable(transfer, "generatedPacs008"));
             variables.put("paymentScheme", "HCT_INST:RECALL");
-            variables.put("recallAdditionalInformation", "");  // TODO take value from UI field
+            variables.put("recallAdditionalInformation", comment);
         } else {
             bpmn = recallBpmnBatch;
             variables.put("originalPacs008", getTransferVariable(transfer, "generatedPacs008Fragment"));
@@ -56,7 +65,7 @@ public class CamundaService {
         variables.put("internalCorrelationId", transactionId);
         variables.put("tenantIdentifier", tenantIdentifier);
         variables.put("originalPacs008TransactionIdentification", transactionId);
-        variables.put("recallReason", "TECH");  // TODO take value from UI field
+        variables.put("recallReason", comment);
         logger.debug("starting BPMN {} for paymentScheme {} using variables: {}", bpmn, paymentScheme, variables);
         zeebeClient.newCreateInstanceCommand()
                 .bpmnProcessId(bpmn)
