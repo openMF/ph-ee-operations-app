4b6368093edd80d6dcdcc5b3a7b842ec90556685
GeluHUN <peter.gelei@dpc.hu>
2024-06-27 15:06:25 +0200

    make recall query worker retryable BCB-3525

--- src/main/java/org/apache/fineract/recall/Ig2OriginalMsgIdForRecall.java
+++ src/main/java/org/apache/fineract/recall/Ig2OriginalMsgIdForRecall.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 
@@ -51,44 +52,83 @@ public class Ig2OriginalMsgIdForRecall implements JobHandler {
     public void handle(JobClient client, ActivatedJob job) {
         logger.info("IG2 incoming recall called getOriginalMsgIdForRecall");
 
-        final Map<String, Object> variables = job.getVariablesAsMap();
-        logger.trace("variables: {}", variables);
-        if (!variables.containsKey("transactionId")) {
-            throw new RuntimeException("transactionId variable not found");
-        }
-        String internalCorrelationId = (String) variables.get("internalCorrelationId");
-
-        logger.debug("searching for original incoming transfer where transactionId is {}", internalCorrelationId);
-        Transfer incomingTransfer = transferRepository.findIcomingTransfersForRecall(internalCorrelationId);
-        if (incomingTransfer == null) {
-            throw new RuntimeException("transfer not found for internalCorrelationId " + internalCorrelationId);
-        }
-        Long workflowInstanceKey = incomingTransfer.getWorkflowInstanceKey();
-        if (logger.isTraceEnabled()) {
-            logger.trace("found original incoming transfer: {}", incomingTransfer);
-        } else {
-            logger.debug("found original incoming transfer with workflowInstanceKey {}", workflowInstanceKey);
-        }
-
-        logger.debug("searching for variable pacs008FileMetadata where workflowInstanceKey is {}", workflowInstanceKey);
-        Optional<Variable> pacs008FileMetadata = variableRepository.findByWorkflowInstanceKeyAndVariableName("pacs008FileMetadata", workflowInstanceKey);
-        if (pacs008FileMetadata.isEmpty()) {
-            throw new RuntimeException("pacs008FileMetadata not found for internalCorrelationId " + internalCorrelationId);
-        }
-
-        String json = pacs008FileMetadata.get().getValue();
-        FileMetaData fileMetaData;
         try {
-            fileMetaData = mapper.readValue(json, FileMetaData.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("failed to parse json from pacs008FileMetadata '" + json + "'", e);
+            final Map<String, Object> variables = job.getVariablesAsMap();
+            logger.trace("variables: {}", variables);
+            if (!variables.containsKey("transactionId")) {
+                throw new RuntimeException("transactionId variable not found");
+            }
+            String internalCorrelationId = (String) variables.get("internalCorrelationId");
+
+            logger.debug("searching for original incoming transfer where transactionId is {}", internalCorrelationId);
+            Transfer incomingTransfer = transferRepository.findIcomingTransfersForRecall(internalCorrelationId);
+            if (incomingTransfer == null) {
+                throw new RuntimeException("transfer not found for internalCorrelationId " + internalCorrelationId);
+            }
+            Long workflowInstanceKey = incomingTransfer.getWorkflowInstanceKey();
+            if (logger.isTraceEnabled()) {
+                logger.trace("found original incoming transfer: {}", incomingTransfer);
+            } else {
+                logger.debug("found original incoming transfer with workflowInstanceKey {}", workflowInstanceKey);
+            }
+
+            logger.debug("searching for variable pacs008FileMetadata where workflowInstanceKey is {}", workflowInstanceKey);
+            Optional<Variable> pacs008FileMetadata = variableRepository.findByWorkflowInstanceKeyAndVariableName("pacs008FileMetadata", workflowInstanceKey);
+            if (pacs008FileMetadata.isEmpty()) {
+                throw new RuntimeException("pacs008FileMetadata not found for internalCorrelationId " + internalCorrelationId);
+            }
+
+            String json = pacs008FileMetadata.get().getValue();
+            FileMetaData fileMetaData;
+            try {
+                fileMetaData = mapper.readValue(json, FileMetaData.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("failed to parse json from pacs008FileMetadata '" + json + "'", e);
+            }
+
+            logger.debug("for the internalCorrelationId {} the originalMessageId found is {}", internalCorrelationId, fileMetaData.messageId());
+
+            client.newCompleteCommand(job)
+                    .variables(Map.of("originalMessageId", fileMetaData.messageId()))
+                    .send()
+                    .whenComplete((response, exception) -> {
+                        if (exception != null) {
+                            logger.error("failed to complete job {}: {}", job.getType(), exception.getMessage(), exception);
+                        }
+                        logger.debug("completed job {}", job.getType());
+                    });
+
+        } catch (Exception e) {
+            if (e.getCause() != null) {
+                logger.error("{} worker failed with: '{}' caused by '{}'", job.getType(), e.getMessage(), e.getCause().getMessage(), e);
+            } else {
+                logger.error("{} worker failed with: '{}'", job.getType(), e.getMessage(), e);
+            }
+
+            if (e instanceof NullPointerException || job.getRetries() <= 0) {
+                client.newThrowErrorCommand(job)
+                        .errorCode("Error_OriginalMsgIdForRecallNotFound")
+                        .send()
+                        .whenComplete((response, exception) -> {
+                            if (exception != null) {
+                                logger.error("failed to throw bpmn error in job {}: {}", job.getType(), exception.getMessage(), exception);
+                                return;
+                            }
+                            logger.debug("thrown bpmn error in job {}", job.getType());
+                        });
+
+            } else {
+                client.newFailCommand(job)
+                        .retries(job.getRetries() - 1)
+                        .retryBackoff(Duration.ofHours(1))
+                        .send()
+                        .whenComplete((response, exception) -> {
+                            if (exception != null) {
+                                logger.error("failed to fail job {}: {}", job.getType(), exception.getMessage(), exception);
+                            }
+                            logger.debug("failed job {}", job.getType());
+                        });
+            }
         }
-
-        logger.debug("for the internalCorrelationId {} the originalMessageId found is {}", internalCorrelationId, fileMetaData.messageId());
-
-        client.newCompleteCommand(job)
-                .variables(Map.of("originalMessageId", fileMetaData.messageId()))
-                .send()
-                .join();
     }
 }
